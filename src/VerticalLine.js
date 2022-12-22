890df2549f25e03908c5d08a4e29cbb320b9085e
tamandrew <atam2088@gmail.com>
2022-12-22 15:07:10 -0500

    Update VerticalLine.js

--- src/VerticalLine.js
+++ src/VerticalLine.js
@@ -1,10 +1,10 @@
 
 const VerticalLine = (props) => {
 
-    return <div onClick = {(e) => {e.stopPropagation();}}className = {"z-0 bg-black/20 absolute"} 
+    return <div onClick = {(e) => {e.stopPropagation();}}className = {"z-0 bg-black/5 absolute"} 
         style = {{
             width: "2px",
-            left: props.x + 12,
+            left: props.x,
             top: 0,
             height: "200vh"
         }} />
